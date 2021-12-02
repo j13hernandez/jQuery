9cb61d64e2866a2d7f8b978458e3d313c5df18d8
Palma <1hernandezj3@gmail.com>
2021-12-01 23:54:40 -0500

    Completion of jquery tutorial

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 $(document).ready(function () {
+  $('h1').addClass('big-title margin');
+  $('h1').removeClass('big-title');
+  $('h1').text('Hello World');
+  $('button').text('WOOP');
+  $('button').html('<em>heeey</em>');
+});
+$('h1').click(function () {
   $('h1').css('color', 'red');
+});
+
+$('button').click(function () {
+  $('h1').slideUp().slideDown();
+});
+
+$('input').keydown(function (event) {
+  if (event.key === 'q') {
+    $('h1').css('color', 'purple');
+    $('h1').text(event.key);
+  } else {
+    $('h1').text(event.key);
+  }
+});
+
+$('h1').on('mouseover', function () {
+  $('h1').css('color', 'green');
 });
